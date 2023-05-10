0e3a4efc196e76b3cdaa92642c8a47331c87b6ea
Rosmontic <129030690+Rosmontic@users.noreply.github.com>
2023-05-10 17:21:15 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -13928,7 +13928,7 @@ T.hKm.prototype = {
             var u, t, s, r = this, q = null, p = r.fy
             if (p == null) {
                 r.fy = a[0].a
-                d.a.push(T.e("[0]对[1]发动[虚假的剑闪]", r.r, q, q, 1, 1000, 100))
+                d.a.push(T.e("[0]发动[虚假的剑闪]", r.r , p, q, q, 1, 1000, 100))
                 r.r.ry.i(0, r.fr)
                 p = r.r
                 p.j = p.j + p.db * 3

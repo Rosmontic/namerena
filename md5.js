48e08f3f62ab99564d32677c1f288b5ad550dfbc
Rosmontic <129030690+Rosmontic@users.noreply.github.com>
2023-05-11 13:10:31 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -13948,10 +13948,9 @@ T.hKm.prototype = {
                 r.ae(0)
                 if (p.fr > 0) {
                     u = d.a
-                    u.push(T.e("[0]发动[真实的斩击]", r.r, p, q, q, 1, 1000, 100))
-d.a.push($.v())
 u.push(T.e("[0]:[我将在此，成为英雄]", r.r, p, q, q, 1, 1000, 100))
-d.a.push($.v())
+u.push($.v())
+                    u.push(T.e("[0]发动[真实的斩击]", r.r, p, q, q, 1, 1000, 100))
                     t = T.u(r.r, !0, c)
                     s = T.u(r.r, !0, c)
 

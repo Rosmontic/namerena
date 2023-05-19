dacb8d985e2332aaa50e8d54ba8343cdcc23a85b
Rosmontic <129030690+Rosmontic@users.noreply.github.com>
2023-05-19 16:57:24 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -20079,13 +20079,12 @@ g.a.push(T.e(C.c.K("[0]免疫", $.nj()), s.r, c, null, null, 20, 1500, 100))
 c.a0(0, !0, s.r, e, f, g)
                 t = s.r
                 t.j = t.j - 480
-a=0
-                return a
+                return 0
             }
             return a
         },
         T: function() {
-            this.r.y1.i(0, this)
+            this.r.x2.i(0, this)
         },
         $iK: 1
     }

0663eb357c9bc2f726c1db23678cacc0bba26e2b
Rosmontic <129030690+Rosmontic@users.noreply.github.com>
2023-05-19 16:45:33 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -20067,27 +20067,20 @@ s.push(u)
         $iK: 1
 }
 T.tha4b1.prototype = {
-        dk: function(a, b, c, d, e, f, g) {
-            var u, t, s = this
-            if (c.fr <= 0)
+        ak: function(a, b, c, d, e) {
+            var u = a > 0
+            if (u ) {
+                e.a.push(T.e("[0][免疫]", this.r, null, null, null, a, 1000, 100))
+a=0
                 return a
-            if (f.m() < s.f && f.m() < 128 && s.r.bq(f) && !c.millionPower) {
-                u = T.u(s.r, !0, f) * 0.5
-                if (u > a)
-                    u = a
-g.a.push(T.e(C.c.K("[0]免疫", $.nj()), s.r, c, null, null, 20, 1500, 100))
-c.a0(0, !0, s.r, e, f, g)
-                t = s.r
-                t.j = t.j - 480
-                return 0
             }
             return a
         },
         T: function() {
-            this.r.x2.i(0, this)
+            this.r.y1.i(0, this)
         },
-        $iK: 1
-    }
+        $iD: 1
+}
     T.i9.prototype = {
         ga1: function() {
             return 10

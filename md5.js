b01d2ebdc4cdf7b4ca2ebc17ff9348fbebda4a49
Rosmontic <129030690+Rosmontic@users.noreply.github.com>
2023-05-22 16:26:49 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -6783,6 +6783,13 @@ tha4n2: function tha4n2() {
             _.f = 0
             _.c = _.b = _.a = _.r = null
         },
+tha6s1t: function tha6s1t() {
+            var _ = this
+            _.e = !1
+            _.f = 0
+            _.c = _.b = _.a = _.r = null
+        },
+
         tha6s1: function tha6s1() {
             var _ = this
              _.fx =_.fr = _.fy = null
@@ -16712,6 +16719,29 @@ aK: function(a, b, c, d) {
             this.fr.C()
         }  
 }
+T.tha6s1t.prototype = {
+        t: function(a, b, c, d) {
+            var u, s, r = this, q = null, l ,n
+            l = a[0].a
+            d.a.push(T.e("[0]使用[炽击「大鹏坠击拳」]", r.r, l, q, q, 1, 1000, 100))
+            for (s = !1,n = 0; n < 3; ++n) {
+                if (r.r.fr > 0 && !r.r.A && l.fr > 0) {
+                    d.a.push($.v())
+                    if (l.fr > 0 && !l.A && T.bd(100 + r.r.cy, l.dx + l.cy, c)) {
+                        if (s)
+                            d.a.push(T.e(O.c("SYdr"), l, r.r, q, q, 0, 1000, 100))
+                        else
+                            d.a.push(T.e(O.c("vVob"), l, r.r, q, q, 0, 1000, 100))
+                        return
+                    }
+                    if (l.aA(l.ak(C.e.Z(T.u(r.r, !1, c)*1.5 / T.cc(l, !1, c)), r.r, T.a3(), c, d), r.r, T.a3(), c, d) > 0)
+                        s = !0
+                    d.a[d.a.length].b = 300
+                    }
+                }
+}
+}
+
     T.tigerFastPunch.prototype = {
         t: function(a, b, c, d) {
             var u, t, s, r, q, p, o, n = this, m = null, l = a[0].a, k = d.a
@@ -18116,10 +18146,13 @@ T.hong.prototype = {
             u = new T.tha6n3()
             u.f = 0
             t.push(u)
+u = new T.tha6s1t()
+            u.f = 200
+            t.push(u)
             u = new T.tha6s1()
 u.fr = new T.bP(u)
 u.fx = new T.cB(u)
-            u.f = 180
+            u.f = 0
             t.push(u)
 }
 }
@@ -21531,7 +21564,7 @@ T.i9m.prototype = {
         t(Z.hl, Z.ax)
         s(F.ab, [T.H, T.M, T.D, T.J, T.L, T.K, T.A, T.G, T.C, T.I])
         s(T.H, [T.y, T.hJ, T.tha5b1, T.hR, T.i0, T.i3, T.dU, T.dZ, T.bU, T.dQ, T.hZ, T.i5, T.dV, T.dX, T.tha4b1,T.tha3b2, T.i9 , T.i9m, T.ih, T.im, T.tha3b1, T.io, T.tigerMillionPunch, T.darkRevive, T.hellDefend])
-        s(T.y, [T.dL, T.hI ,T.tha6n1, T.hK, T.hKm, T.bx, T.hL, T.hM, T.dN, T.dO, T.dP, T.tha3n1, T.tha3n2, T.tha3s1, T.tha6n2, T.tha6n3, T.tha5n1, T.hS, T.hU, T.hV, T.bV, T.dR, T.hX, T.tha4n3, T.dS, T.tha4n1, T.tha4n2, T.dT, T.i1, T.i6, T.i8, T.dW, T.ia ,T.tha4s1, T.i7, T.ig, T.ij, T.hW, T.ik, T.il, T.tha5n3, T.tha6s1, T.dM, T.tha5n2, T.tha5s1, T.hO, T.dc, T.hQ, T.i_, T.bI, T.i2, T.i4, T.ie, T.aK, T.bm, T.bn, T.hT, T.dY, T.ic, T.tigerAttack, T.tigerCheckHP, T.tigerIceAttack, T.tigerBlastPunch, T.tigerBlade, T.tigerRecover, T.tigerFastPunch, T.tigerWindAttack, T.tigerBlastKick, T.darkRecover, T.darkFastAttack, T.darkIron, T.darkHaste, T.darkLock, T.darkDemonSword, T.darkAttack, T.hellFireAttack, T.hellWindAttack, T.hellCutter, T.hellBehead, T.hellCheckHP])
+        s(T.y, [T.dL, T.hI ,T.tha6n1, T.hK, T.hKm, T.bx, T.hL, T.hM, T.dN, T.dO, T.dP, T.tha3n1, T.tha3n2, T.tha3s1, T.tha6n2, T.tha6n3, T.tha5n1, T.hS, T.hU, T.hV, T.bV, T.dR, T.hX, T.tha4n3, T.dS, T.tha4n1, T.tha4n2, T.dT, T.i1, T.i6, T.i8, T.dW, T.ia ,T.tha4s1, T.i7, T.ig, T.ij, T.hW, T.ik, T.il, T.tha5n3, T.tha6s1t,T.tha6s1, T.dM, T.tha5n2, T.tha5s1, T.hO, T.dc, T.hQ, T.i_, T.bI, T.i2, T.i4, T.ie, T.aK, T.bm, T.bn, T.hT, T.dY, T.ic, T.tigerAttack, T.tigerCheckHP, T.tigerIceAttack, T.tigerBlastPunch, T.tigerBlade, T.tigerRecover, T.tigerFastPunch, T.tigerWindAttack, T.tigerBlastKick, T.darkRecover, T.darkFastAttack, T.darkIron, T.darkHaste, T.darkLock, T.darkDemonSword, T.darkAttack, T.hellFireAttack, T.hellWindAttack, T.hellCutter, T.hellBehead, T.hellCheckHP])
         s(T.M, [T.bf, T.co, T.dj , T.cG, T.aN, T.hz, T.darkHasteRun, T.darkSuperUpgrade])
         s(T.p, [T.dt, T.aQ, T.ii, T.h0, T.iT])
         s(T.w, [T.dC, T.av, T.cA, T.hc, T.hj, T.hk, T.he, T.cy])

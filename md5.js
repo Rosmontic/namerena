0dcf325864eee479041afb3909e5e78c397ca124
Rosmontic <129030690+Rosmontic@users.noreply.github.com>
2023-05-22 18:50:02 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -16722,11 +16722,11 @@ aK: function(a, b, c, d) {
 }
        T.tha6s1t.prototype = {
         t: function(a, b, c, d) {
-            var u, s, r = this, q = null, l ,n
+            var u, s, r = this, q = null, l ,n ,m
             l = a[0].a
             d.a.push(T.e("[0]使用[炽击「大鹏坠击拳」]", r.r, l, q, q, 1, 1000, 100))
             for (s = !1,
-n = 0; n < 3; ++n) {
+            n = 0; n < 3; ++n) {
                 if (r.r.fr > 0 && !r.r.A && l.fr > 0) {
                     d.a.push($.v())
                     if (l.fr > 0 && !l.A && T.bd(100 + r.r.cy, l.dx + l.cy, c)) {
@@ -16736,7 +16736,8 @@ n = 0; n < 3; ++n) {
                             d.a.push(T.e("[0]2", l, r.r, q, q, 0, 1000, 100))
                         return
                     }
-                    if (l.aA(l.ak(C.e.Z(T.u(r.r, !1, c)*1.5 / T.cc(l, !1, c)), r.r, T.a3(), c, d), r.r, T.a3(), c, d) > 0){
+                    m = T.u(n.r, !0, c)
+                    if (l.aA(l.ak(C.e.Z( m *1.5 / T.cc(l, !0, c)), r.r, T.a3(), c, d), r.r, T.a3(), c, d) > 0){
                         s = !0}
                     d.a[d.a.length].b = 300
                     }

906152307631d101996f86fde248ed1d693a1f45
Rosmontic <129030690+Rosmontic@users.noreply.github.com>
2023-05-24 13:53:19 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -17154,8 +17154,8 @@ T.tha7s1.prototype = {
             n.push(a[u].a)
         t = "[0]使用「梦中的死灵之书」]"
         s = this.r
-        this.time ++
-        if (this.time > 2)
+        s.time ++
+        if (s.time > 1)
         {
             this.f = 0
         }

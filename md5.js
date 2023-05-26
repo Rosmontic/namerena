1efe22c8d0bbcf4076d747b5782f7d98bbff5ffe
Rosmontic <129030690+Rosmontic@users.noreply.github.com>
2023-05-26 09:44:55 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -4748,14 +4748,14 @@
             }
         },
         sls: function(a, b, c, d, e) {
-             var u, t
+            var u, t
             if (c > 0 && !(b.fr <= 0)) {
                 if (b.a6($.b8(), d))
                     return
                 u = b.r1
                 t = H.o(u.h(0, $.b8()), "$idj")
                 if (t == null) {
-                    t = new T.dj(b)
+                    t = new T.sl(b)
                     t.x = new T.hs(t)
                     u.k(0, $.b8(), t)
                     b.r2.i(0, t)
@@ -4767,6 +4767,27 @@
                     t.y += 2048
                 u = T.e(C.c.K(O.c("lZqU"), $.ng()), a, b, null, null, 40, 1000, 100)
                 e.a.push(u)
+            }
+        },
+
+        pe: function(a, b, c, d, e) {
+            var u, t
+            if (c > 4 && !(b.fr <= 0)) {
+                if (b.a6($.b9(), d))
+                    return
+                u = b.r1
+                t = H.o(u.h(0, $.b9()), "$idE")
+                if (t == null) {
+                    t = new T.dE(a,b)
+                    t.y = T.u(a, !0, d) * 1.2
+                    u.k(0, $.b9(), t)
+                    b.x1.i(0, t)
+                } else {
+                    t.y = t.y + T.u(a, !0, d) * 1.2
+                    t.z = 4
+                    t.r = a
+                }
+                e.a.push(T.e(C.c.K(O.c("UAjR"), $.ni()), a, b, null, null, 60, 1000, 100))
             }
         },
         u: function(a, b, c) {
@@ -15360,6 +15381,7 @@ o.a0(p, !0, this.r, T.a3(), c, d)
                 t = a[0].a
         s = T.u(u.r, !1, c)
         d.a.push(T.e("[0]使用[水木符「水精灵」]", u.r, t, null, null, 1, 1000, 100))
+        u.r.element1 = 2
         t.a0(0.75*s, !0, u.r, T.sla(), c, d)
         d.a.push($.v())
         t.a0(0.75*s, !0, u.r, T.a3(), c, d)
@@ -16258,7 +16280,6 @@ T.tha4n1.prototype = {
             }
         },
         $ip: 1
-
     }
     T.dT.prototype = {
         a8: function(a, b, c) {
@@ -22104,7 +22125,7 @@ T.i9m.prototype = {
         s(F.ab, [T.H, T.M, T.D, T.J, T.L, T.K, T.A, T.G, T.C, T.I])
         s(T.H, [T.y, T.hJ, T.tha5b1, T.tha6b1, T.tha7b1, T.hR, T.i0, T.i3, T.dU, T.dZ, T.bU, T.dQ, T.hZ, T.i5, T.dV, T.dX, T.tha4b1,T.tha3b2, T.i9 , T.i9m, T.ih, T.im, T.tha3b1, T.io, T.tigerMillionPunch, T.darkRevive, T.hellDefend])
         s(T.y, [T.dL, T.hI ,T.tha6n1, T.hK, T.hKm, T.bx, T.hL, T.hM, T.dN, T.dO, T.dP, T.tha3n1, T.tha3n2, T.tha3s1, T.tha6n2, T.tha6n3, T.tha5n1, T.tha7n1 ,T.tha7n2,T.tha8n1 ,T.tha8t1 ,T.tha8n2 ,T.tha8t2 ,T.tha8n3 ,T.tha8t3 ,T.tha8n4 ,T.tha8t4 ,T.tha8n5 ,T.tha8t5 ,T.tha8s1 , T.hS, T.hU, T.hV, T.bV, T.dR, T.hX, T.tha4n3, T.dS, T.tha4n1, T.tha4n2, T.dT, T.i1, T.i6, T.i8, T.dW, T.ia ,T.tha4s1, T.i7, T.ig, T.ij, T.hW, T.ik, T.il, T.tha5n3, T.tha6s1t,T.tha6s1, T.dM, T.tha5n2, T.tha5s1, T.tha7s1, T.hO, T.dc, T.hQ, T.i_, T.bI, T.i2, T.i4, T.ie, T.aK, T.bm, T.bn, T.hT, T.dY, T.ic, T.tigerAttack, T.tigerCheckHP, T.tigerIceAttack, T.tigerBlastPunch, T.tigerBlade, T.tigerRecover, T.tigerFastPunch, T.tigerWindAttack, T.tigerBlastKick, T.darkRecover, T.darkFastAttack, T.darkIron, T.darkHaste, T.darkLock, T.darkDemonSword, T.darkAttack, T.hellFireAttack, T.hellWindAttack, T.hellCutter, T.hellBehead, T.hellCheckHP])
-        s(T.M, [T.bf, T.co, T.dj , T.cG, T.aN, T.hz, T.darkHasteRun, T.darkSuperUpgrade])
+        s(T.M, [T.bf, T.co, T.dj ,T.sl , T.cG, T.aN, T.hz, T.darkHasteRun, T.darkSuperUpgrade])
         s(T.p, [T.dt, T.aQ, T.ii, T.h0, T.iT])
         s(T.w, [T.dC, T.av, T.cA, T.hc, T.hj, T.hk, T.he, T.cy])
         s(T.D, [T.cg, T.dF, T.el])

4831ce504ab14b8ebe04235399e32d8abfed4c09
Rosmontic <129030690+Rosmontic@users.noreply.github.com>
2023-06-08 10:43:14 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -7530,6 +7530,12 @@ tha5b1: function tha5b1() {
             _.f = 0
             _.c = _.b = _.a = _.r = null
         },
+		tha12b1: function tha12b1() {
+            var _ = this
+            _.e = !1
+            _.f = 0
+            _.c = _.b = _.a = _.r = null
+        },
         tigerCheckHP: function tigerCheckHP() {
             var _ = this
             _.e = !1
@@ -16135,7 +16141,7 @@ T.tha2ak.prototype = {
 
 T.tha2n1.prototype = {
     ar: function(a, b) {
-            if (this.r.bagua > 2+ C.d.v((a.m() & 3) || this.r.bagua < 1)
+            if (this.r.bagua > 2+ (a.m() & 3) || this.r.bagua < 1)
             { 
                 return !1
             }
@@ -16403,7 +16409,7 @@ T.tha8t2.prototype = {
 			
 			m = q.fr
             p.push(T.e("[0]使用[水木符「水精灵」]", s.r, q, r, r, 1, 1000, 100))
-			u.r.element2 = 1
+			s.r.element2 = 1
 			for(n=0 ; n<2 ; ++n ){
 			
             if (!q.a6($.aC(), c))
@@ -19438,6 +19444,16 @@ T.tha7b1.prototype = {
     },
     $iD: 1
 }
+T.tha12b1.prototype = {
+    ak: function(a, b, c, d, e) {
+        var u = a > 0 
+        return u && J.Q(c, T.mA()) ? 0 : 0
+    },
+    T: function() {
+        this.r.y1.i(0, this)
+    },
+    $iD: 1
+}
     T.tigerCheckHP.prototype = {
         ak: function(a, b, c, d, e) {
 			if(this.r.fr - a <= 180 && !this.r.upgrade) {
@@ -21083,15 +21099,15 @@ T.rin.prototype = {
             return C.rin
         },
         gaQ: function() {
-            return H.a([], [P.f])
+            return H.a([$.c3(), $.ai(), $.am(), $.d4(), $.aB(), $.c4(), $.aC(), $.b8()], [P.f])
         },
         aa: function() {
             var u, t = new T.aK()
             t.r = this
             this.k2 = t
             t = this.id 
-
-
+			t.push(new T.tha12b1())
+           
 }
 }
 T.reimu.prototype = {
@@ -24682,7 +24698,7 @@ T.i9m.prototype = {
         t(P.cD, P.l)
         t(Z.hl, Z.ax)
         s(F.ab, [T.H, T.M, T.D, T.J, T.L, T.K, T.A, T.G, T.C, T.I])
-        s(T.H, [T.y, T.hJ, T.tha1b1, T.tha5b1, T.tha6b1, T.tha7b1, T.tha10b1, T.hR, T.i0, T.i3, T.dU, T.dZ, T.bU, T.dQ, T.hZ, T.i5, T.thj7b1, T.dV, T.dX, T.tha4b1,T.tha3b2, T.i9 , T.i9m, T.ih, T.tha9b1,T.tha1b2, T.im, T.tha3b1, T.io, T.tigerMillionPunch, T.darkRevive, T.hellDefend])
+        s(T.H, [T.y, T.hJ, T.tha1b1, T.tha5b1, T.tha6b1, T.tha7b1, T.tha10b1,T.tha12b1, T.hR, T.i0, T.i3, T.dU, T.dZ, T.bU, T.dQ, T.hZ, T.i5, T.thj7b1, T.dV, T.dX, T.tha4b1,T.tha3b2, T.i9 , T.i9m, T.ih, T.tha9b1,T.tha1b2, T.im, T.tha3b1, T.io, T.tigerMillionPunch, T.darkRevive, T.hellDefend])
         s(T.y, [T.dL, T.hI ,T.tha6n1, T.tha10t1, T.hK, T.hKm, T.bx, T.hL, T.hM, T.dN, T.dO, T.dP, T.tha2n1, T.tha2ak, T.tha2n2, T.tha2n3, T.tha2b1, T.tha1n3, T.tha3n1, T.tha3n2, T.tha3s1, T.tha6n2, T.tha6n3, T.tha5n1, T.tha7n1 ,T.tha7n2,T.tha8n1 ,T.tha8t1 ,T.tha8n2 ,T.tha8t2 ,T.tha8n3 ,T.tha8t3 ,T.tha8n4 ,T.tha8t4 ,T.tha8n5 ,T.tha8t5 ,T.tha8s1 ,T.tha9n1,T.tha9n3,T.tha10n1,T.tha10n2,T.tha10s1,T.tha11n1,T.tha11n2, T.tha11n1t, T.tha11n2t, T.tha11t1, T.hS, T.hU, T.tha1n2, T.hV, T.bV, T.dR, T.hX, T.tha4n3, T.dS, T.tha4n1, T.tha4n2, T.tha4n2x, T.tha7t1, T.dT, T.i1, T.tha1n1, T.i6, T.i8, T.dW, T.ia ,T.tha4s1, T.i7, T.ig, T.tha9n2, T.tha9n2t, T.ij,T.tha1s1, T.tha9s1, T.hW, T.ik, T.il, T.tha5n3, T.tha6s1t,T.tha6s1, T.tha11s1,T.tha2s1, T.dM, T.tha5n2, T.tha5s1, T.tha7s1, T.hO, T.dc, T.hQ, T.i_, T.bI, T.i2, T.i4, T.ie, T.aK, T.bm, T.bn, T.hT, T.dY, T.ic, T.tigerAttack, T.tigerCheckHP, T.tigerIceAttack, T.tigerBlastPunch, T.tigerBlade, T.tigerRecover, T.tigerFastPunch, T.tigerWindAttack, T.tigerBlastKick, T.darkRecover, T.darkFastAttack, T.darkIron, T.darkHaste, T.darkLock, T.darkDemonSword, T.darkAttack, T.hellFireAttack, T.hellWindAttack, T.hellCutter, T.hellBehead, T.hellCheckHP])
         s(T.M, [T.bf, T.co, T.dj ,T.sl , T.cG, T.aN, T.hz, T.darkHasteRun, T.darkSuperUpgrade])
         s(T.p, [T.dt, T.aQ, T.ii, T.h0, T.iT])
@@ -25052,7 +25068,7 @@ C.daiyousei = H.a(u([0, 17, 28, 1, 2, 15, 35, 1000]), [P.j])
 C.cirno = H.a(u([-6, 29, 16, 17, 29, 1, 57, 565]), [P.j])
 C.hong = H.a(u([33, 26, 12, 7, -16, 1, 28, 708]), [P.j])
 C.koakuma = H.a(u([0, 9, 19, -3, 7, 32, 31, 700]), [P.j])
-C.patchouli = H.a(u([21, -5, -20, 19, 26, 19, 39, 438]), [P.j])
+C.patchouli = H.a(u([21, -5, -20, 19, 36, 49, 39, 438]), [P.j])
 C.sakuya = H.a(u([-9, 23, 6, 36, 3, 29, 57, 474]), [P.j])
 C.nue = H.a(u([47, 32, 26, 51, 59, 37, 13, 791]), [P.j])
 C.reimu = H.a(u([31, 38, 15, 16, 44, 25, 32, 704]), [P.j])

f59518f0668332267d89ba6935ecb9d255bf18ad
Rosmontic <129030690+Rosmontic@users.noreply.github.com>
2023-09-03 16:30:09 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -2491,7 +2491,7 @@
         V: function(a, b) {
             P.pt(a, b)
         },
-        a6: function(a, b) {
+        
             b.bJ(0, a)
         },
         a5: function(a, b) {
@@ -13308,6 +13308,12 @@ tha4b1: function tha4b1() {
             _.f = 0
             _.c = _.b = _.a = _.r = null
         },
+        ribbit4b3: function ribbit4b3() {
+            var _ = this
+            _.e = !1
+            _.f = 0
+            _.c = _.b = _.a = _.r = null
+        },
 		thb6b1: function thb6b1() {
             var _ = this
             _.e = !1
@@ -21374,13 +21380,14 @@ T.thb9t2.prototype = {
         s = T.u(u.r, !1, c)      
 		v = 0.5 + u.r.yin
 		if(u.r.yin > 1)
-		{
+		{        
+        d.a.push(T.e("[0]发起狙击", u.r, t, null, null, 0, 1000, 100))
         t.bL(v*s, !0, u.r, T.a3(), c, d)
-		d.a.push(T.e("[0]：[命中...目标...]", u.r, t, null, null, 1, 1000, 100))
+		d.a.push(T.e("[0]：[命中...目标...]", u.r, t, null, null, 0, 1000, 100))
 		}
 		else{
+        d.a.push(T.e("[0]发起狙击", u.r, t, null, null, 0, 1000, 100))
 		t.a0(v*s, !0, u.r, T.a3(), c, d)
-		d.a.push(T.e("[0]：[命中...目标...]", u.r, t, null, null, 1, 1000, 100))
 		}
 		
 		
@@ -24011,7 +24018,7 @@ n.r.bf(n.r.fr, null, c, d)
             return H.a(["berserk", "charm", "ice", "poison", "curse", "fire", "disperse", "half", "exchange"], [P.f])
         },
         aa: function() {
-            var u = new T.ribbit1t1()
+            var u = new T.noak()
             u.r = this
             this.k2 = u
         },
@@ -24043,7 +24050,7 @@ n.r.bf(n.r.fr, null, c, d)
             return H.a(["berserk", "charm", "ice", "poison", "curse", "fire", "disperse", "half", "exchange"], [P.f])
         },
         aa: function() {
-            var u = new T.aK()
+            var u = new T.ribbit1t1()
             u.r = this
             this.k2 = u
         },
@@ -26720,7 +26727,7 @@ T.ribbit1t1.prototype = {
             q = d.a
             q.push(T.e(k, s, null, null, r, 1, 1000, 100))           
             o = a[0].a
-            if (o.cy + o.cx> (c.m() & 255))
+            if (o.cy + o.cx> (c.m() & 255)+ 129)
             {
             d.a.push(T.e("[0]闪避了效果", o, o, null, null, 0, 1000, 100))
             return
@@ -31181,7 +31188,10 @@ T.yuzu.prototype = {
         u.f = 100
         t.push(u)
         u =new T.ribbit4b2()
-        u.f = 1000
+        u.f = 100
+        t.push(u)
+        u =new T.ribbit4b3()
+        u.f = 100
         t.push(u)
     }, 
     bM: function() {
@@ -33838,6 +33848,25 @@ dk: function(a, b, c, d, e, f, g) {
         },
         $iK: 1
     }
+    T.ribbit4b3.prototype = {
+        dk: function(a, b, c, d, e, f, g) {
+            var v ,u, t, s = this
+            if (c.fr <= 0)
+                return a
+            v = 128 + this.r.yin * 16
+            if (f.m() < v && s.r.bq(f)) {
+                g.a.push(T.e(C.c.K("[0]：藏起来了！", $.nj()), s.r, c, null, null, 20, 1500, 100))
+                t = s.r
+                t.j = t.j
+                return 0
+            }
+            return a
+        },
+        T: function() {
+            this.r.x2.i(0, this)
+        },
+        $iK: 1
+    }
 	T.thb1b1.prototype = {
         dk: function(a, b, c, d, e, f, g) {
             var u, t, s, r = this ,v
@@ -34187,9 +34216,11 @@ dk: function(a, b, c, d, e, f, g) {
                 t = r.f
                 s = u.x
                 if (t >= s)
+                {
                     v = (c.au(1 + C.b.v(t * 3, 4)) + 1)
                     u.x = s + v
                     d.a.push(T.e("[0]获得[1]点[护盾]", this.r , v, null, null, 0, 1000, 100))
+                }
                     d.a.push(T.e("[0]拥有[1]点[护盾]", this.r , u.x, null, null, 0, 1000, 100))
                 }
             return a
@@ -35471,7 +35502,7 @@ dk: function(a, b, c, d, e, f, g) {
         t(P.cD, P.l)
         t(Z.hl, Z.ax)
         s(F.ab, [T.H, T.M, T.D, T.J, T.L, T.K, T.A, T.G, T.C, T.I])
-        s(T.H, [T.y, T.youzi3, T.tao3, T.lv3, T.hJ, T.tha1b1, T.ribbit4b1, T.tha5b1, T.tha6b1, T.tha7b1, T.thb7b2, T.thb9b2,T.thb9b1,  T.tha10b1,T.tha12b1, T.thb5b2, T.thb7b1, T.ribbit4n1, T.hR, T.i0, T.i3, T.dU, T.dZ, T.bU, T.dQ, T.hZ, T.i5, T.thj7b1, T.dV, T.dX, T.tha4b1,T.tha3b2,T.thb1b1,T.thb6b1, T.i9 ,T.hifumib1 ,T.ribbit3b1 ,T.baals1b1 ,T.baals1b2 , T.i9m, T.thb7b3 , T.ih, T.kg, T.ribbit4b2, T.thb5b1, T.thb6b2, T.tha9b1,T.tha1b2, T.thb2b1 , T.im, T.tha3b1, T.io, T.tigerMillionPunch, T.darkRevive, T.hellDefend])
+        s(T.H, [T.y, T.youzi3, T.tao3, T.lv3, T.hJ, T.tha1b1, T.ribbit4b1, T.tha5b1, T.tha6b1, T.tha7b1, T.thb7b2, T.thb9b2,T.thb9b1,  T.tha10b1,T.tha12b1, T.thb5b2, T.thb7b1, T.ribbit4n1, T.ribbit4b3, T.hR, T.i0, T.i3, T.dU, T.dZ, T.bU, T.dQ, T.hZ, T.i5, T.thj7b1, T.dV, T.dX, T.tha4b1,T.tha3b2,T.thb1b1,T.thb6b1, T.i9 ,T.hifumib1 ,T.ribbit3b1 ,T.baals1b1 ,T.baals1b2 , T.i9m, T.thb7b3 , T.ih, T.kg, T.ribbit4b2, T.thb5b1, T.thb6b2, T.tha9b1,T.tha1b2, T.thb2b1 , T.im, T.tha3b1, T.io, T.tigerMillionPunch, T.darkRevive, T.hellDefend])
         s(T.y, [T.dL, T.ribbit4a1, T.ribbit1n0, T.fox1n0, T.yuzun1, T.yuzun2, T.momorin1, T.momorin2, T.midorin1, T.midorin2, T.hifumia1, T.szja1, T.noak, T.youzi1, T.tao1, T.lv1, T.youzi2, T.tao2, T.lv2, T.hI ,T.tha6n1, T.thb8t1b, T.tha10t1, T.hK, T.hKm,T.thb9s1, T.thb7s1, T.baals1n2,  T.bx, T.hL, T.hM, T.dN, T.tha6t2, T.dO, T.dP, T.tha2n1, T.tha2ak, T.tha12ak, T.tha2n2, T.tha2n3, T.tha2b1, T.tha1n3, T.tha3n1, T.ba1a1, T.ba1n2, T.baals1n1, T.arisn1, T.arisn2, T.thb1n1, T.thb1s1, T.thb1n2, T.thb1n3, T.thb8t1a, T.thb2n1, T.thb2n2 ,T.thb2n3, T.thb2s1, T.thb2n1t, T.thb2n2t ,T.thb2n3t ,  T.tha3n2, T.tha3s1, T.tha6n2, T.tha6n3, T.tha5n1, T.tha7n1 ,T.tha7n2,T.tha8n1 ,T.tha8t1 ,T.tha8n2 ,T.tha8t2 ,T.tha8n3 ,T.tha8t3 ,T.tha8n4 ,T.tha8t4 ,T.tha8n5 ,T.tha8t5 ,T.tha8s1 , T.ribbit3n2, T.tha9n1,T.tha9n3,T.tha10n1,T.tha10n2,T.tha10s1,T.tha11n1,T.tha11n2, T.tha11n1t, T.tha11n2t, T.tha11t1, T.thb3n1,T.thb3n2,T.thb3n3,T.thb3n4,T.thb3n5,T.thb3n6,T.thb3n7,T.thb3n8,T.thb3n9,T.thb3s1 ,T.thb3s2 ,T.thb9t1 ,T.thb9t2 ,T.thb8n1, T.thb8t1 ,T.thb8t2 ,T.thb3t1,T.thb3t2,T.thb3t3,T.thb3t4,T.thb3t5,T.thb3t6,T.thb3t7,T.thb3t8, T.hS, T.thb4b1, T.thb4b2, T.hU, T.tha1n2, T.ba1n1 ,T.hV, T.bV, T.dR, T.hX, T.thb8s1, T.tha4n3, T.thb4n1, T.thb4n2,T.thb4s1, T.thb5s1, T.thb5n1, T.thb5n2, T.thb5n3, T.dS, T.tha4n1, T.tha4n2, T.baals1n3, T.baals1n5, T.baals1n6, T.baals1n4, T.hifumin1, T.hifumin2, T.ribbit1n2, T.ribbit3n1, T.fox4n1, T.tha4n2x, T.tha7t1, T.dT, T.i1, T.tha1n1, T.thb9n1, T.i6, T.i8, T.dW, T.ia ,T.tomb1 ,T.tha4s1, T.i7, T.ig, T.tha9n2, T.tha9n2t,T.yeye, T.ij, T.ribbit4n2, T.ribbit1t1, T.tha1s1, T.tha9s1, T.hW, T.hWalf, T.ik, T.il, T.tha5n3, T.thb9n2, T.tha6s1t, T.tha6s1, T.thb6n1, T.thb6n2, T.thb6n3, T.thb6s1, T.thb7n1, T.thb7t1, T.thb7n2, T.tha11s1, T.tha2s1, T.dM, T.tha5n2, T.tha5s1, T.tha7s1, T.hO, T.dc, T.hQ, T.i_, T.bI, T.i2, T.i4, T.ie, T.aK, T.bm, T.bn, T.hT, T.dY, T.ic, T.tigerAttack, T.tigerCheckHP, T.tigerIceAttack, T.tigerBlastPunch, T.tigerBlade, T.tigerRecover, T.tigerFastPunch, T.tigerWindAttack, T.tigerBlastKick, T.darkRecover, T.darkFastAttack, T.darkIron, T.darkHaste, T.darkLock, T.darkDemonSword, T.darkAttack, T.hellFireAttack, T.hellWindAttack, T.hellCutter, T.hellBehead, T.hellCheckHP])
         s(T.M, [T.bf, T.co, T.dj ,T.sl , T.cG, T.sD, T.aN, T.hz, T.shenx, T.lockx, T.blindlyx, T.darkHasteRun, T.darkSuperUpgrade])
         s(T.p, [T.dt, T.aQ, T.ii, T.h0, T.iT])

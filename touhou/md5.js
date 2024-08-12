0d717a7c6e3d5da8bbb93daeb08bb22690dd73ae
Rosmontic <129030690+Rosmontic@users.noreply.github.com>
2024-08-12 17:03:54 +0800

    Update md5.js

--- touhou/md5.js
+++ touhou/md5.js
@@ -8731,6 +8731,12 @@
 				_.f = 0
 				_.c = _.b = _.a = _.r = null
 			},
+			milkyholmes: function milkyholmes() {
+				var _ = this
+				_.e = !1
+				_.f = 0
+				_.c = _.b = _.a = _.r = null
+			},
 			dL: function dL() {
 				var _ = this
 				_.e = !1
@@ -12413,8 +12419,9 @@
 				_.id = e
 				_.resist = 0
 				_.relief = 0
-				_.dodge = 10
+				_.dodge = 0
 				_.milky = 0
+				_.holmes = []
 				_.k2 = _.k1 = null
 				_.k3 = f
 				_.k4 = null
@@ -12455,8 +12462,9 @@
 				_.id = e
 				_.resist = 0
 				_.relief = 0
-				_.dodge = 10
+				_.dodge = 0
 				_.milky = 0
+				_.holmes = []
 				_.k2 = _.k1 = null
 				_.k3 = f
 				_.k4 = null
@@ -12497,8 +12505,9 @@
 				_.id = e
 				_.resist = 30
 				_.relief = 0
-				_.dodge = 95
+				_.dodge = 15
 				_.milky = 0
+				_.holmes = []
 				_.k2 = _.k1 = null
 				_.k3 = f
 				_.k4 = null
@@ -12539,8 +12548,9 @@
 				_.id = e
 				_.resist = 15
 				_.relief = 15
-				_.dodge = 10
+				_.dodge = 0
 				_.milky = 0
+				_.holmes = []
 				_.k2 = _.k1 = null
 				_.k3 = f
 				_.k4 = null
@@ -19911,11 +19921,58 @@
 						o.bL(p, !0, this.r, T.a3(), c, d)
 					}
 				}
-
 			}
-
 		}
 	}
+	T.milkyholmes.prototype = {
+		b5: function(a) {
+			return a.b1(this.r.x.a.e)
+		},
+		a8: function(a, b, c) {
+			return c.gbh() * a.J
+		},
+		gb3: function() {
+			return 999
+		},
+		gb4: function() {
+			return 999
+		},
+		ar: function(a, b) {
+			if (this.r.milky != 0) {
+				return !1
+			}
+			return this.aS(a, b)
+		},
+		t: function(a, b, c, d) {
+			var m, u, t, s, r, q, p, o, n = H.a([], [T.aU])
+			for (u = 0; u < a.length; ++u)
+				n.push(a[u].a)
+			t = "[0]：[milkyholmes，集结]"
+			s = this.r
+			r = H.a(n.slice(0), [H.n(n, 0)])
+			q = d.a
+			this.r.milky = 1
+			q.push(T.e(t, s, null, null, r, 1, 1000, 100))
+			for (m = 0 ;u = 0; u < n.length; ++u) {
+				o = n[u]
+				if (o.fr > 0 && o.milky != null) {
+					this.r.holmes[m] = o
+					++m			
+				}
+			}
+			if (holmes.length > 0){
+				for (m = 0 ;u = 0; u < holmes.length; ++u) {
+					o = holmes[u]
+					o.milky = 1
+					o.holmes = this.r.holmes
+					o.holmes[u] = this.r
+					q.push(T.e(t, s, null, null, r, 0, 1000, 100))
+				}
+			}	
+			this.r.j = this.r.j + 2024
+		}
+	}
+
 	T.dL.prototype = {
 		ar: function(a, b) {
 			var u
@@ -24497,6 +24554,10 @@
 			t.r = this
 			this.k2 = t
 			t = this.id
+			u = new T.milkyholmes()
+			u.f = 1024
+			t.push(u)
+
 		}
 	}
 	T.nero.prototype = {
@@ -24508,6 +24569,10 @@
 			t.r = this
 			this.k2 = t
 			t = this.id
+			u = new T.milkyholmes()
+			u.f = 1024
+			t.push(u)
+
 		}
 	}
 	T.gray.prototype = {
@@ -24519,6 +24584,10 @@
 			t.r = this
 			this.k2 = t
 			t = this.id
+			u = new T.milkyholmes()
+			u.f = 1024
+			t.push(u)
+
 		}
 	}
 	T.poirot.prototype = {
@@ -24530,6 +24599,9 @@
 			t.r = this
 			this.k2 = t
 			t = this.id
+			u = new T.milkyholmes()
+			u.f = 1024
+			t.push(u)
 		}
 	}
 	T.targeta.prototype = {
@@ -28294,7 +28366,7 @@
 		t(Z.hl, Z.ax)
 		s(F.ab, [T.H, T.M, T.D, T.J, T.L, T.K, T.A, T.G, T.C, T.I])
 		s(T.H, [T.y, T.hJ, T.hR, T.i0, T.i3, T.dU, T.dZ, T.bU, T.dQ, T.hZ, T.i5, T.dV, T.dX, T.i9, T.ih, T.targetready, T.reikirecover, T.im, T.io, T.tigerMillionPunch, T.darkRevive, T.hellDefend, T.hellTaichiReflect, T.wargodSay])
-		s(T.y, [T.dL, T.hI, T.hK, T.bx, T.dance, T.hL, T.hM, T.dN, T.dO, T.dP, T.hS, T.hU, T.hV, T.bV, T.dR, T.hX, T.dS, T.dT, T.i1, T.i6, T.i8, T.dW, T.ia, T.i7, T.oddloop, T.ig, T.ij, T.hW, T.ik, T.il, T.dM, T.hO, T.dc, T.hQ, T.i_, T.bI, T.i2, T.i4, T.ie, T.aK, T.noaK, T.bm, T.bn, T.hT, T.dY, T.ic, T.tigerAttack, T.tigerCheckHP, T.tigerIceAttack, T.tigerBlastPunch, T.tigerBlade, T.tigerRecover, T.tigerFastPunch, T.tigerWindAttack, T.tigerBlastKick, T.darkRecover, T.darkFastAttack, T.darkIron, T.darkHaste, T.darkLock, T.darkDemonSword, T.darkAttack, T.hellFireAttack, T.hellWindAttack, T.hellCutter, T.hellBehead, T.hellCheckHP, T.hellWargod, T.hellAttack, T.hellSword, T.hellHeartbreakSword, T.hellEclipseSword, T.wargodSweepAttack, T.wargodCritical, T.wargodAttack, T.ironbirdLaser, T.ironbirdCheckHP, T.ironbirdSimulate, T.ironbirdForceShield, T.robotForceShield, T.ironbirdUpgrade, T.ironbirdRepair, T.thaK, T.thdf, T.tha1n1, T.tha1n2, T.tha1n3, T.tha1s1])
+		s(T.y, [T.dL, T.hI, T.hK, T.bx, T.dance, T.hL, T.hM, T.dN, T.dO, T.dP, T.hS, T.hU, T.hV, T.bV, T.dR, T.hX, T.dS, T.dT, T.i1, T.i6, T.i8, T.dW, T.ia, T.i7, T.oddloop, T.ig, T.ij, T.hW, T.ik, T.il, T.dM, T.hO, T.dc, T.hQ, T.i_, T.bI, T.i2, T.i4, T.ie, T.aK, T.noaK, T.bm, T.bn, T.hT, T.dY, T.ic, T.tigerAttack, T.tigerCheckHP, T.tigerIceAttack, T.tigerBlastPunch, T.tigerBlade, T.tigerRecover, T.tigerFastPunch, T.tigerWindAttack, T.tigerBlastKick, T.darkRecover, T.darkFastAttack, T.darkIron, T.darkHaste, T.darkLock, T.darkDemonSword, T.darkAttack, T.hellFireAttack, T.hellWindAttack, T.hellCutter, T.hellBehead, T.hellCheckHP, T.hellWargod, T.hellAttack, T.hellSword, T.hellHeartbreakSword, T.hellEclipseSword, T.wargodSweepAttack, T.wargodCritical, T.wargodAttack, T.ironbirdLaser, T.ironbirdCheckHP, T.ironbirdSimulate, T.ironbirdForceShield, T.robotForceShield, T.ironbirdUpgrade, T.ironbirdRepair, T.thaK, T.thdf, T.tha1n1, T.tha1n2, T.tha1n3, T.tha1s1, T.milkyholmes])
 		s(T.M, [T.bf, T.co, T.dj, T.cG, T.seal, T.aN, T.hz, T.darkHasteRun, T.darkSuperUpgrade, T.ironbirdSimulateRun])
 		s(T.p, [T.dt, T.aQ, T.ii, T.h0, T.iT])
 		s(T.w, [T.dC, T.av, T.cA, T.hc, T.hj, T.hk, T.he, T.cy])

4ddec7e118855bb18347913285982a2be6abecff
Rosmontic <129030690+Rosmontic@users.noreply.github.com>
2024-08-13 14:43:33 +0800

    Update md5.js

--- touhou/md5.js
+++ touhou/md5.js
@@ -19991,11 +19991,11 @@
 			var n, m, u, t, s, r = this
 			n = c.m()
 			m = c.m()
+			d.a.push(T.e("[0]:啊咧", this.r, this.r, null, null, 0, 1000, 100))
 			if (r.f > 0) {
 				if (m < 2 && this.r.toys == 1){
 					this.r.toys = 0
 					n = 0
-					d.a.push(T.e("[0]:啊咧", this.r, this.r, null, null, 0, 1000, 100))
 					if (this.r.holmes.length > 0){
 						for(u = 0; u < this.r.holmes.length; u++){
 							if (this.r.holmes[u].toys = 1){
